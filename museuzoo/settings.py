9269313cc2b02147d377f7a0247dd3a18eabda35
aescobarr <agustiescobar@gmail.com>
2017-11-10 15:34:02 +0100

    Changed website name

--- museuzoo/settings.py
+++ museuzoo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2*ye+cg)_6!8itlrm$744r67^xjmoxfa7m81f-+1!qgno7pr)='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['montesdata.creaf.cat']
+ALLOWED_HOSTS = ['geomuseu.creaf.cat']
 
 
 # Application definition
