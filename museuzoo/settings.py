00281c35805719257ce4d91f2b9d852eabb3c89e
aescobarr <agustiescobar@gmail.com>
2017-11-23 09:28:09 +0100

    Celery logging results to database

--- museuzoo/settings.py
+++ museuzoo/settings.py
@@ -190,7 +190,8 @@ FORCE_LOWERCASE_TAGS = True
 # CELERY STUFF
 BROKER_URL = 'redis://localhost:6379'
 #CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'django-db'
+#CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
